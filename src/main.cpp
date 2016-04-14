86652923bb9d7f98ee565347bfe165ab58f5eb7d
Kurt Kaminski <kurtoon@gmail.com>
2016-04-13 17:02:34 -0700

    updating OmniRender, trying to render cube map

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "omConfig.hpp"
 #include "omTexture.hpp"
 #include "omShader.hpp"
+#include "omFBO.hpp"
 
 using namespace al;
 using namespace om;
@@ -12,6 +13,8 @@ using namespace om;
 struct MyApp : public App {
 
   Config omniConfig;
+  om::FBO fbo;
+  om::CubeMap cubeMap;
   std::vector<om::Texture> tex;
   om::ShaderProgram captureShader, warpShader;
   Mesh mesh;
@@ -28,15 +31,10 @@ struct MyApp : public App {
     omniConfig.loadData("tmp");
     omniConfig.printConfig();
 
-    // //scale the data
-    // for (auto &i: omniConfig.mProjector) {
-    //   for (int j = 0; j < i.width*i.height; j++) {
-    //     i.data[4*j+0] *= .1;
-    //     i.data[4*j+1] *= .1;
-    //     i.data[4*j+2] *= .1;
-    //     i.data[4*j+3] *= .1;
-    //   }
-    // }
+    //Set up cube map and fbo
+    cubeMap.init(1024);
+    fbo.init(800,400);
+    fbo.attach(cubeMap);
 
     //Load textures
     for (auto& i : omniConfig.mProjector){
@@ -45,8 +43,15 @@ struct MyApp : public App {
     }
 
     //Init shader
-    captureShader.load("OmniRender/include/omCapture.vert", "OmniRender/include/omCapture.frag");
-    // warpShader.load("OmniRender/include/omWarp.vert", "OmniRender/include/omWarp.frag");
+    captureShader.load("OmniRender/include/omCapture.vert", "OmniRender/include/omCapture.frag", true);
+    warpShader.load("OmniRender/include/omWarp.vert", "OmniRender/include/omWarp.frag");
+
+    glUseProgram(warpShader.program);
+    GLint h = glGetUniformLocation(warpShader.program, "warpMap");
+    GLint i = glGetUniformLocation(warpShader.program, "cubeMap");
+    glUniform1i(h, 0);
+    glUniform1i(i, 1);
+    warpShader.validate();
 
     addOctahedron(mesh);
   }
@@ -66,7 +71,7 @@ struct MyApp : public App {
     g.depthMask(0); // write only to color buffer
 
       // QUAD PART
-      tex.back().bind();
+      // tex.back().bind();
       Mesh& m = g.mesh();
       m.reset();
       m.primitive(g.TRIANGLE_STRIP);
@@ -79,7 +84,7 @@ struct MyApp : public App {
         m.texCoord	( 1, 1);
         m.vertex	(1, 1, 0);
       g.draw(m);
-      tex.back().unbind();
+      // tex.back().unbind();
 
     g.depthMask(1);
     g.popMatrix(g.PROJECTION);
@@ -87,7 +92,6 @@ struct MyApp : public App {
   }
 
   virtual void onDraw( Graphics& g ) override {
-    // drawQuad(g);
 
     glUseProgram(captureShader.program);
 
@@ -106,11 +110,34 @@ struct MyApp : public App {
     glUniform1f(f, 0.0);
     glUniform1f(g2, 0.0);
 
+    fbo.bind();
     for (int i = 0; i < 6; i++){
       glUniform1i(c, i);
-      g.draw(mesh);
-    }
+      glFramebufferTexture2D( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                              GL_TEXTURE_CUBE_MAP_POSITIVE_X+i, cubeMap.id(), 0);
 
+      // g.pushMatrix();
+        // g.translate(i,0,0);
+        // glColor4f((float)1/i,0,1,1);
+        g.draw(mesh);
+      // g.popMatrix();
+    }
+    fbo.unbind();
+    fbo.checkStatus();
+
+
+    // Draw fbo result
+    glActiveTexture(GL_TEXTURE0);
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, tex.back().id());
+
+    glActiveTexture(GL_TEXTURE1);
+    glEnable(GL_TEXTURE_CUBE_MAP);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap.id());
+
+    glUseProgram(warpShader.program);
+
+    drawQuad(g);
 
   }
 
